145d02ce7761d548efb6e213fdd92cbf8535fc08
win laptop <mysql_query@hotmail.com.tr>
2023-04-20 02:24:04 +0300

    Modified

---
+++ satoshiturk/redis.go
@@ -0,0 +1,36 @@
+package satoshiturk
+
+import (
+	"context"
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+)
+
+func CheckKeyExists(address common.Address) (bool, error) {
+	// Redis bağlantısı oluştur
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // Redis şifresi, eğer varsa
+		DB:       0,  // Seçilecek Redis veritabanı
+	})
+
+	// Adresi string'e dönüştür
+	addressStr := address.Hex()
+
+	// Redis üzerinde anahtarı ara
+	keyExists, err := rdb.Exists(context.Background(), addressStr).Result()
+	if err != nil {
+		// Hata oluştu
+		return false, err
+	}
+
+	if keyExists == 1 {
+		// Anahtar mevcut
+		fmt.Printf("Key '%s' exists in Redis\n", addressStr)
+		return true, nil
+	} else {
+		// Anahtar mevcut değil
+		fmt.Printf("Key '%s' does not exist in Redis\n", addressStr)
+		return false, nil
+	}
+}
